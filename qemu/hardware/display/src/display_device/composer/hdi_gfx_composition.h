2b4c307974acfbb4535855be48cd69656b7f9484
Francesco Pham <francesco.pham1@h-partners.com>
2025-02-28 13:45:37 +0100

    qemu: remove display_gfx and stub gfx composition
    Signed-off-by: Francesco Pham <francesco.pham@huawei.com>

--- qemu/hardware/display/src/display_device/composer/hdi_gfx_composition.h
+++ qemu/hardware/display/src/display_device/composer/hdi_gfx_composition.h
@@ -13,40 +13,26 @@
  * limitations under the License.
  */
 
-#ifndef HDI_GFX_COMPOSITION_H
-#define HDI_GFX_COMPOSITION_H
-#include "display_gfx.h"
-#include "hdi_composer.h"
-namespace OHOS {
-namespace HDI {
-namespace DISPLAY {
-class HdiGfxComposition : public HdiComposition {
-public:
-    int32_t Init(void) override;
-    int32_t SetLayers(std::vector<HdiLayer *> &layers, HdiLayer &clientLayer) override;
-    int32_t Apply(bool modeSet) override;
-    ~HdiGfxComposition() override
-    {
-        (void)GfxModuleDeinit();
-    }
-
-private:
-    bool CanHandle(HdiLayer &hdiLayer);
-    void InitGfxSurface(ISurface &surface, HdiLayerBuffer &buffer);
-    int32_t BlitLayer(HdiLayer &src, HdiLayer &dst);
-    int32_t ClearRect(HdiLayer &src, HdiLayer &dst);
-    int32_t GfxModuleInit(void);
-    int32_t GfxModuleDeinit(void);
-    void *mGfxModule = nullptr;
-    GfxFuncs *mGfxFuncs = nullptr;
-    HdiLayer *mClientLayer;
-    static constexpr const char* LIB_HDI_GFX_NAME = "libdisplay_gfx.z.so";
-    static constexpr const char* LIB_GFX_FUNC_INIT = "GfxInitialize";
-    static constexpr const char* LIB_GFX_FUNC_DEINIT = "GfxUninitialize";
-    bool valid_ = false;
-};
-} // namespace OHOS
-} // namespace HDI
-} // namespace DISPLAY
-
-#endif // HDI_GFX_COMPOSITION_H
+ #ifndef HDI_GFX_COMPOSITION_H
+ #define HDI_GFX_COMPOSITION_H
+ #include "display_gfx.h"
+ #include "hdi_composer.h"
+ namespace OHOS {
+ namespace HDI {
+ namespace DISPLAY {
+ class HdiGfxComposition : public HdiComposition {
+ public:
+     int32_t Init(void) override;
+     int32_t SetLayers(std::vector<HdiLayer *> &layers, HdiLayer &clientLayer) override;
+     int32_t Apply(bool modeSet) override;
+     ~HdiGfxComposition() override {}
+ 
+ private:
+     HdiLayer *mClientLayer;
+ };
+ } // namespace OHOS
+ } // namespace HDI
+ } // namespace DISPLAY
+ 
+ #endif // HDI_GFX_COMPOSITION_H
+ 
